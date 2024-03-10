1dce0562c64c4604d11a7108ea64270265dc84d1
Rafael Silva <rafa.306@hotmail.com>
2024-03-10 11:36:27 -0300

    commit inicial

---
+++ src/test/java/br/com/rssilva/curso_aws_project01/CursoAwsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rssilva.curso_aws_project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoAwsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
